9cb3540314234fc92ac89d3f45abbb7d2e68cc56
Wang-biolab <100891966+Wang-biolab@users.noreply.github.com>
2022-03-19 22:31:28 +0800

    Add files via upload

---
+++ 1-ReadData/readDATA.R
@@ -0,0 +1,15 @@
+library(Seurat)
+rm(list=ls())
+dir <- dir("RawData/")
+dir <- paste0("RawData/", dir)
+#查看文件顺序
+dir                         
+#按文件顺序给样本命名，名称不要以数字开头，中间不能有空格 
+group = c('p1','p2','p3','t1','t2','t3','rb1','rb2','rb3','rn1','rn2','rn3','rt1','rt2','rt3')
+
+##使用循环命令批量创建seurat对象
+for(i in 1:length(dir)){
+  counts <- Read10X(data.dir = dir[i])
+  scRNA <- CreateSeuratObject(counts, project= group[i])
+  save(scRNA, file= paste('RawData/scRNA_',group[i],'.Rdata',sep=''))}
+  
