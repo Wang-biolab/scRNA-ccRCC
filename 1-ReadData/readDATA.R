83a270020cd51577643b085c45453fe3957371be
Wang-biolab <100891966+Wang-biolab@users.noreply.github.com>
2022-03-21 15:03:09 +0800

    Add files via upload

--- 1-ReadData/readDATA.R
+++ 1-ReadData/readDATA.R
@@ -1,23 +1,37 @@
+# load packages
 library(Seurat)
+library(tidyverse)
 rm(list=ls())
+
+
 dir <- dir("RawData/")
 dir <- paste0("RawData/", dir)
-#查看文件顺序
-dir                         
-#按文件顺序给样本命名，名称不要以数字开头，中间不能有空格 
+
+# Name the samples in file order（do not start the name with a number, and have no spaces in between）
 group = c('p1','p2','p3','t1','t2','t3','rb1','rb2','rb3','rn1','rn2','rn3','rt1','rt2','rt3')
 
-##使用循环命令批量创建seurat对象
+# Create seurat objects in batches using loop command
 for(i in 1:length(dir)){
   counts <- Read10X(data.dir = dir[i])
   scRNA <- CreateSeuratObject(counts, project= group[i])
   save(scRNA, file= paste('RawData/scRNA_',group[i],'.Rdata',sep=''))}
 
+scRNAlist <- list()
+for(i in 1:length(dir)){
+  counts <- Read10X(data.dir = dir[i],gene.column = 1)
+  counts <- as.data.frame(counts)
+  mat <- counts[rowSums(counts)!=0,]
+  rm(counts)
 
-#合并 seurat 对象
-scRNA <- merge(x = seob_list[[1]], #第一个
-              y = seob_list[-1], #其他的
-              add.cell.ids = names(seob_list) #cell id 添加前缀
-              )
+  scRNAlist[[i]] <- CreateSeuratObject(mat,
+                                       project=group[i],
+                                       min.cells=3,
+                                       min.features = 200)
+  scRNAlist[[i]] <- RenameCells(scRNAlist[[i]], add.cell.id = group[i])
+}
 
-  
+
+# Name the list and save the data
+names(scRNAlist) <- group
+scRNA <- merge(scRNAlist[[1]],
+               y= scRNAlist[2:length(scRNAlist)])
