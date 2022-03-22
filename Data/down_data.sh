ecc14afe1b07ae9d7670d99727c8cd082b4ff8a4
Wang-biolab <100891966+Wang-biolab@users.noreply.github.com>
2022-03-22 15:48:24 +0800

    Add files via upload

---
+++ Data/down_data.sh
@@ -0,0 +1,6 @@
+## GSE121636
+wget https://ftp.ncbi.nlm.nih.gov/geo/series/GSE139nnn/GSE139555/suppl/GSE139555_RAW.tar
+
+## GSE139555
+wget https://ftp.ncbi.nlm.nih.gov/geo/series/GSE139nnn/GSE139555/suppl/GSE139555_RAW.tar
+
